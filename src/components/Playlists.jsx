2f0b1920235ee8f94e27d27ea7bbdeaa981541cd
souhaibzarai <souhaibzarai18@gmail.com>
2025-11-20 10:19:00 +0100

    - Search for songs. - Adding song to a playlist.

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 import { useMusic } from "../hooks/useMusicContext";
 
 export const Playlists = () => {
-  const { playlists, createPlaylist, allSongs } = useMusic();
+  const {
+    playlists,
+    createPlaylist,
+    addSongToPlaylist,
+    deletePlaylist,
+    allSongs,
+    handlePlaySong,
+    currentTrackId,
+  } = useMusic();
   const [newPlaylistName, setNewPlaylistName] = useState("");
 
   const [selectedPlaylist, setSelectedPlaylist] = useState(null);
@@ -13,6 +21,25 @@ export const Playlists = () => {
     if (newPlaylistName.trim()) {
       createPlaylist(newPlaylistName.trim());
       setNewPlaylistName("");
+    }
+  };
+
+  const clearInput = () => {
+    setSearchQuery("");
+    setSelectedPlaylist(null);
+    setShowDropdown(false);
+  };
+
+  const handleAddSongToPlaylist = (song) => {
+    if (selectedPlaylist) {
+      addSongToPlaylist(selectedPlaylist.id, song);
+      clearInput();
+    }
+  };
+
+  const deletePlaylistConfirmation = (playlist) => {
+    if (window.confirm(`Are you sure you want to delete "${playlist.name}"`)) {
+      deletePlaylist(playlist.id);
     }
   };
 
@@ -56,7 +83,12 @@ export const Playlists = () => {
               <div className="playlist-header">
                 <h3 className="playlist-name">{playlist.name}</h3>
                 <div className="playlist-actions">
-                  <button className="delete-playlist-btn">Delete</button>
+                  <button
+                    className="delete-playlist-btn"
+                    onClick={() => deletePlaylistConfirmation(playlist)}
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
               <div className="add-song-section">
@@ -78,19 +110,60 @@ export const Playlists = () => {
                       setShowDropdown(e.target.value.length > 0);
                     }}
                   />
+
                   {selectedPlaylist?.id === playlist.id && showDropdown && (
                     <div className="song-dropdown">
                       {filteredSongs.length === 0 ? (
-                        <div className="dropdown-item no-results">
+                        <div
+                          className="dropdown-item no-results"
+                          onClick={clearInput}
+                        >
                           No songs found...
                         </div>
                       ) : (
-                        <div className="dropdown-item">
-                          {filteredSongs.length}
-                        </div>
+                        filteredSongs.slice(0, 5).map((song) => {
+                          return (
+                            <div
+                              key={song.id}
+                              className="dropdown-item"
+                              onClick={() => handleAddSongToPlaylist(song)}
+                            >
+                              <div className="song-info">
+                                <span className="song-title">{song.title}</span>
+                                <span className="song-artist">
+                                  {song.artist}
+                                </span>
+                              </div>
+                            </div>
+                          );
+                        })
                       )}
                     </div>
                   )}
+                  <div className="songs-section">
+                    {playlist.songs.length === 0 ? (
+                      <div className="empty-playlist">
+                        No songs in this playlist
+                      </div>
+                    ) : (
+                      playlist.songs.map((song) => {
+                        return (
+                          <div
+                            className={`playlist-song-info ${
+                              currentTrackId === song.id ? "active" : ""
+                            }`}
+                            onClick={() => {
+                              clearInput();
+                              handlePlaySong(song, song.id);
+                            }}
+                          >
+                            <span className="song-title">{song.title}</span>
+                            <span className="song-artist">{song.artist}</span>
+                          </div>
+                        );
+                      })
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
